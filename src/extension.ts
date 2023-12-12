1227af63330af73bd237adbb3d2b3a17a66310d1
Paolo Chang <chanhwan.chang@gmail.com>
2023-12-12 00:19:52 -0500

    Removed frogs from old comments

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,132 @@ import { isEmpty, startsWith } from "lodash";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+
+  function registerCommand(command: string, color: string = "") {
+    return vscode.commands.registerCommand(command, () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const document = editor.document;
+        const fullText = document.getText();
+        const newText = fullText.replace(
+          /console\.log\("%c🐸/g,
+          'console.log("%c'
+        );
+
+        const selection = editor.selection;
+        const variableName = editor.document.getText(selection);
+
+        // Calculate position for inserting the log statement
+        const [newLine, numIndent] = getConsolePosition(editor);
+
+        // Create a WorkspaceEdit to combine both operations into a single edit
+        const workspaceEdit = new vscode.WorkspaceEdit();
+
+        // Perform both operations within the same WorkspaceEdit
+        workspaceEdit.replace(
+          document.uri,
+          new vscode.Range(0, 0, document.lineCount, 0),
+          newText
+        );
+        workspaceEdit.insert(
+          document.uri,
+          new vscode.Position(newLine, 0),
+          getLogStatement(numIndent, variableName, color)
+        );
+
+        // Apply the combined edit to the workspace
+        vscode.workspace.applyEdit(workspaceEdit);
+      }
+    });
+  }
+
+  function getLogStatement(
+    numIndent: number,
+    variableName: string,
+    color: string
+  ) {
+    const statement = [];
+    const newIndent = " ".repeat(numIndent);
+    statement.push(newIndent);
+    statement.push(`console.log("%c🐸 ${variableName}:", ${variableName}`);
+    if (!isEmpty(color)) {
+      statement.push(`, "${color}"`);
+    }
+    statement.push(");\n");
+    return statement.join("");
+  }
+
+  // Return pure code without comments
+  function getCleanCode(input: string): string {
+    let text = input;
+    let withinString = false;
+
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] === '"' || text[i] === "'") {
+        withinString = !withinString;
+      }
+
+      if (!withinString && text.slice(i).startsWith("//")) {
+        text = text.slice(0, i).trim();
+        break;
+      }
+    }
+
+    return text;
+  }
+
+  function getEndLine(
+    editor: vscode.TextEditor,
+    lineNumber: number
+  ): [number, number] {
+    let char = "";
+    let code = "";
+    let lineCount = lineNumber;
+    let lineIndent = 0;
+
+    while (
+      char !== ";" &&
+      char !== "{" &&
+      char !== ":" &&
+      !startsWith(code, "return")
+    ) {
+      const next = editor.document.lineAt(lineCount);
+      code = getCleanCode(next.text).trim();
+      char = code.charAt(code.length - 1);
+      lineIndent = next.firstNonWhitespaceCharacterIndex;
+
+      lineCount += 1;
+      if (char === "{" || char === ":") {
+        const startLine = editor.document.lineAt(lineCount);
+        if (startsWith(startLine.text.trimStart(), "case")) {
+          char = "";
+        }
+        lineIndent = startLine.firstNonWhitespaceCharacterIndex;
+      }
+    }
+
+    return [lineCount, lineIndent];
+  }
+
+  function getConsolePosition(editor: vscode.TextEditor): [number, number] {
+    const selection = editor.selection;
+    const lineNumber = selection.active.line;
+
+    const line = editor.document.lineAt(lineNumber);
+
+    const validCode = getCleanCode(line.text);
+
+    if (startsWith(validCode.trimStart(), "return")) {
+      return [lineNumber, line.firstNonWhitespaceCharacterIndex];
+    }
+
+    return getEndLine(editor, lineNumber);
+  }
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   const consoleYellow = registerCommand("frog.consoleYellow", "color:yellow");
@@ -27,116 +153,3 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
-
-// The command has been defined in the package.json file
-// Now provide the implementation of the command with registerCommand
-// The commandId parameter must match the command field in package.json
-function registerCommand(command: string, color: string = "") {
-  return vscode.commands.registerCommand(command, () => {
-    const editor = vscode.window.activeTextEditor;
-
-    if (editor) {
-      const selection = editor.selection;
-      const variableName = editor.document.getText(selection);
-
-      // Get the current position and line number
-      const position = editor.selection.active;
-
-      // Calculate position for inserting the log statement
-      const [newLine, numIndent] = getConsolePosition(editor);
-
-      const newPosition = position.with(newLine, 0);
-
-      // Construct the log statement
-      const logStatement = getLogStatement(numIndent, variableName, color);
-
-      // Insert the log statement at the calculated position
-      editor.edit((editBuilder) => {
-        editBuilder.insert(newPosition, logStatement);
-      });
-    }
-  });
-}
-
-function getLogStatement(
-  numIndent: number,
-  variableName: string,
-  color: string
-) {
-  const statement = [];
-  const newIndent = " ".repeat(numIndent);
-  statement.push(newIndent);
-  statement.push(`console.log("%c🐸 ${variableName}:", ${variableName}`);
-  if (!isEmpty(color)) {
-    statement.push(`, "${color}"`);
-  }
-  statement.push(");\n");
-  return statement.join("");
-}
-
-// Return pure code without comments
-function getCleanCode(input: string): string {
-  let text = input;
-  let withinString = false;
-
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === '"' || text[i] === "'") {
-      withinString = !withinString;
-    }
-
-    if (!withinString && text.slice(i).startsWith("//")) {
-      text = text.slice(0, i).trim();
-      break;
-    }
-  }
-
-  return text;
-}
-
-function getEndLine(
-  editor: vscode.TextEditor,
-  lineNumber: number
-): [number, number] {
-  let char = "";
-  let code = "";
-  let lineCount = lineNumber;
-  let lineIndent = 0;
-
-  while (
-    char !== ";" &&
-    char !== "{" &&
-    char !== ":" &&
-    !startsWith(code, "return")
-  ) {
-    const next = editor.document.lineAt(lineCount);
-    code = getCleanCode(next.text).trim();
-    char = code.charAt(code.length - 1);
-    lineIndent = next.firstNonWhitespaceCharacterIndex;
-
-    lineCount += 1;
-    if (char === "{" || char === ":") {
-      const startLine = editor.document.lineAt(lineCount);
-      if (startsWith(startLine.text.trimStart(), "case")) {
-        char = "";
-      }
-      lineIndent = startLine.firstNonWhitespaceCharacterIndex;
-    }
-  }
-
-  return [lineCount, lineIndent];
-}
-
-function getConsolePosition(editor: vscode.TextEditor): [number, number] {
-  const selection = editor.selection;
-  const lineNumber = selection.active.line;
-
-  const line = editor.document.lineAt(lineNumber);
-
-  const validCode = getCleanCode(line.text);
-
-  if (startsWith(validCode.trimStart(), "return")) {
-    return [lineNumber, line.firstNonWhitespaceCharacterIndex];
-  }
-
-  return getEndLine(editor, lineNumber);
-}
